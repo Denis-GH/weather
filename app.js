a4d1955b4fcc526273d5476b26fef2ebe54a6d57
Denis Vakulenko <00vakulenko@gmail.com>
2024-06-06 13:46:37 +0500

    add swiper; add week weather

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ async function getWeatherMain(city) {
     const data = await response.json()
 
     if (data.cod === 200) {
-        document.querySelector(".today__city").innerHTML = data.name
+        document.querySelectorAll(".city").forEach(item => item.innerHTML = data.name)
         document.querySelector(".main-today__temp").innerHTML = Math.round(data.main.temp)
         document.querySelector(".humidity__value").innerHTML = Math.round(data.main.humidity)
         document.querySelector(".visibility__value").innerHTML = Math.round(data.visibility / 1000)
@@ -31,6 +31,7 @@ function searchHandle() {
     if (searchInput.value) {
         getWeatherMain(searchInput.value)
         getWeatherHourly(searchInput.value)
+        getWeatherWeek(searchInput.value)
     } else {
         alert('enter a city')
     }
@@ -56,22 +57,66 @@ async function getWeatherHourly(city) {
     
     const response = await fetch(apiUrl)
     const data = await response.json()
-
-    const hourlyTimes = document.querySelectorAll(".hourly__time");
-    const hourlyTemps = document.querySelectorAll(".hourly__temp");
-    const hourlyIcons = document.querySelectorAll(".hourly__icon");
+    
+    const hourlyTemps = document.querySelectorAll(".hourly__temp")
+    const hourlyTimes = document.querySelectorAll(".hourly__time")
+    const hourlyIcons = document.querySelectorAll(".hourly__icon")
     
     const timezone = data.city.timezone
-
+    
     for (let i = 0; i < 8; i++) {
         hourlyTimes[i].innerHTML = new Date((data.list[i].dt + timezone) * 1000).toUTCString().slice(17, 22)
         hourlyTemps[i].innerHTML = Math.round(data.list[i].main.temp)
         hourlyIcons[i].src = `https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png`
     }
-
 }
 
 for (let i = 0; i < 8; i++) {
     const item = templateHourly.content.cloneNode(true)
     hourlyCards.append(item)
+}
+
+// swiper
+
+const swiper = new Swiper('.swiper', {
+})
+
+// неделя
+
+const weekCards = document.querySelector(".week")
+const templateWeek = document.querySelector("#template-week")
+
+async function getWeatherWeek(city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`
+    
+    const response = await fetch(apiUrl)
+    const data = await response.json()
+    
+    const weekDates = document.querySelectorAll(".week__date")
+    const weekTimes = document.querySelectorAll(".week__time")
+    const weekIcons = document.querySelectorAll(".week__icon")
+    const weekTemps = document.querySelectorAll(".week__temp")
+    const weekPressures = document.querySelectorAll(".week__pressure")
+    const weekWinds = document.querySelectorAll(".week__wind")
+    const weekHumidities = document.querySelectorAll(".week__humidity")
+    
+    const timezone = data.city.timezone
+    
+    for (let i = 0; i < 5; i++) {
+        weekDates[i].innerHTML = new Date((data.list[i * 8].dt + timezone) * 1000).toUTCString().slice(0, 12)
+    }
+    for (let i = 0; i < 20; i++) {
+        weekTimes[i].innerHTML = new Date((data.list[i * 2].dt + timezone) * 1000).toUTCString().slice(17, 22)
+        weekIcons[i].src = `https://openweathermap.org/img/wn/${data.list[i * 2].weather[0].icon}@2x.png`
+        weekTemps[i].innerHTML = Math.round(data.list[i * 2].main.temp)
+        weekPressures[i].innerHTML = Math.round(data.list[i * 2].main.pressure / 1.333)
+        weekWinds[i].innerHTML = Math.round(data.list[i * 2].wind.speed)
+        weekHumidities[i].innerHTML = Math.round(data.list[i * 2].main.humidity)
+    }
+    
+}   
+
+for (let i = 0; i < 5; i++) {
+    const item = templateWeek.content.cloneNode(true)
+    weekCards.append(item)
 }
